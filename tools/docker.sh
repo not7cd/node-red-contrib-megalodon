1f0325020af800d88e18a24502ba5ad4429d0b63
Norbert Szulc <norbert@icetek.io>
2024-06-14 01:39:04 +0200

    chore: readonly for dev env config

--- tools/docker.sh
+++ tools/docker.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 docker run -it --rm -p 1880:1880 \
-    -v ./tools/settings.js:/data/settings.js \
-    -v ./tools/package.json:/data/package.json \
+    -v ./tools/settings.js:/data/settings.js:ro \
+    -v ./tools/package.json:/data/package.json:ro \
     -v .:/data/node_modules/node-red-contrib-megalodon:ro \
     --name node-red-contrib-megalodon nodered/node-red
