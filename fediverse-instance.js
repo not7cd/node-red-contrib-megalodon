0ff33e251a3559d0168e0e87094813269f93a091
Norbert Szulc <norbert@icetek.io>
2024-06-13 22:51:37 +0200

    style: format

--- fediverse-instance.js
+++ fediverse-instance.js
@@ -1,22 +1,19 @@
-module.exports = function(RED) {
-    function InstanceClient(n) {
-        // Node  to create the shared connection to Mastodon
-        RED.nodes.createNode(this,n);
-        this.api_url = n.apiurl;
-        this.access_token = this.credentials.accesstoken;
-        var megalodon = require('megalodon');
+module.exports = function (RED) {
+  function InstanceClient (n) {
+    // Node  to create the shared connection to Mastodon
+    RED.nodes.createNode(this, n)
+    this.api_url = n.apiurl
+    this.access_token = this.credentials.accesstoken
+    var megalodon = require('megalodon')
 
-        this.client = megalodon.default('mastodon', this.api_url, this.access_token);
+    this.client = megalodon.default('mastodon', this.api_url, this.access_token)
+    console.log('Instance client created, API URL: ' + this.api_url)
+  }
+  RED.nodes.registerType('fediverse-instance', InstanceClient, {
+    credentials: {
+      accesstoken: {
+        type: 'password'
+      }
     }
-    RED.nodes.registerType(
-        "fediverse-instance",
-        InstanceClient,
-        {
-            credentials: {
-                accesstoken: {
-                    type: "password"
-                }
-            }
-        }
-    );
+  })
 }
--- post-status.js
+++ post-status.js
@@ -1,30 +1,35 @@
-var megalodon = require('megalodon');
+var megalodon = require('megalodon')
 
-module.exports = function(RED) {
-    function PostStatusNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        this.instance = RED.nodes.getNode(config.instance)
-        var client = megalodon.default('mastodon', this.instance.api_url, this.instance.access_token);
-        node.on('input', function(msg) {
-            var post = msg.payload.toLowerCase();
-            client.postStatus(post)
-                .then(function(res) {
-                    console.log(res.data);
-                    if (res.data.error) {
-                        node.status({fill:"red",shape:"dot",text:res.data.error})
-                    }
-                    msg.payload = res.data;
-                    node.send(msg);
-                })
-                .then(function() {
-                    node.status({fill:"green",shape:"dot",text:"posted"});
-                })
-                .catch(function(error) {
-                    console.error(error);
-                    node.status({fill:"red",shape:"dot",text:error.message});
-                });
-        });
-    }
-    RED.nodes.registerType("post-status",PostStatusNode);
+module.exports = function (RED) {
+  function PostStatusNode (config) {
+    RED.nodes.createNode(this, config)
+    var node = this
+    this.instance = RED.nodes.getNode(config.instance)
+    var client = megalodon.default(
+      'mastodon',
+      this.instance.api_url,
+      this.instance.access_token
+    )
+    node.on('input', function (msg) {
+      var post = msg.payload.toLowerCase()
+      client
+        .postStatus(post)
+        .then(function (res) {
+          console.log(res.data)
+          if (res.data.error) {
+            node.status({ fill: 'red', shape: 'dot', text: res.data.error })
+          }
+          msg.payload = res.data
+          node.send(msg)
+        })
+        .then(function () {
+          node.status({ fill: 'green', shape: 'dot', text: 'posted' })
+        })
+        .catch(function (error) {
+          console.error(error)
+          node.status({ fill: 'red', shape: 'dot', text: error.message })
+        })
+    })
+  }
+  RED.nodes.registerType('post-status', PostStatusNode)
 }
