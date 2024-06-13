f4e4914454dc758586b7ff344a18ca4ab9c766fc
Norbert Szulc <norbert@icetek.io>
2024-06-13 22:50:26 +0200

    fix: use instance config and show status

--- post-status.js
+++ post-status.js
@@ -4,20 +4,25 @@ module.exports = function(RED) {
     function PostStatusNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
-        var BASE_URL = 'https://mastodon.social';
-        var access_token = '...'; // replace with your access token
-        var client = megalodon.default('mastodon', BASE_URL, access_token);
-
+        this.instance = RED.nodes.getNode(config.instance)
+        var client = megalodon.default('mastodon', this.instance.api_url, this.instance.access_token);
         node.on('input', function(msg) {
             var post = msg.payload.toLowerCase();
             client.postStatus(post)
                 .then(function(res) {
                     console.log(res.data);
+                    if (res.data.error) {
+                        node.status({fill:"red",shape:"dot",text:res.data.error})
+                    }
                     msg.payload = res.data;
                     node.send(msg);
                 })
+                .then(function() {
+                    node.status({fill:"green",shape:"dot",text:"posted"});
+                })
                 .catch(function(error) {
                     console.error(error);
+                    node.status({fill:"red",shape:"dot",text:error.message});
                 });
         });
     }
