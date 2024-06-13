40290970a740057686e8e29f55fc2caa6feba909
Norbert Szulc <norbert@icetek.io>
2024-06-13 23:42:13 +0200

    feat: get node

---
+++ get.js
@@ -0,0 +1,41 @@
+var megalodon = require('megalodon')
+
+module.exports = function (RED) {
+  function GetNode (config) {
+    RED.nodes.createNode(this, config)
+    var node = this
+    this.instance = RED.nodes.getNode(config.instance)
+    var client = megalodon.default(
+      'mastodon',
+      this.instance.api_url,
+      this.instance.access_token
+    )
+    node.on('input', function (msg) {
+      var post = msg.payload
+      client
+        .lookupAccount(post)
+        .then(function (res) {
+          console.log(res.data)
+          if (res.data.error) {
+            node.status({ fill: 'red', shape: 'dot', text: res.data.error })
+          }
+          msg.payload = res.data
+          node.send(msg)
+          node.status({
+            fill: 'green',
+            shape: 'dot',
+            text: 'get success'
+          })
+        })
+        .catch(function (error) {
+          console.error(error)
+          node.status({
+            fill: 'red',
+            shape: 'dot',
+            text: error.response.data.error || error.message
+          })
+        })
+    })
+  }
+  RED.nodes.registerType('get', GetNode)
+}
