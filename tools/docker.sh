3788d832e96e65a29ab20a042c0c2b9fe4d22d45
Norbert Szulc <norbert@icetek.io>
2024-06-14 21:23:59 +0200

    chore: add persistent storage for dev env

--- tools/docker.sh
+++ tools/docker.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 
 docker run -it --rm -p 1880:1880 \
+    -v ./tmp:/data \
     -v ./tools/settings.js:/data/settings.js:ro \
     -v ./tools/package.json:/data/package.json:ro \
     -v .:/data/node_modules/node-red-contrib-megalodon:ro \
