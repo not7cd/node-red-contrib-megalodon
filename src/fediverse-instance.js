1f155d597df15190e780531995654aa1aac84761
Norbert Szulc <norbert@icetek.io>
2024-06-14 21:23:19 +0200

    fix: respect chosen SNS kind

--- src/fediverse-instance.js
+++ src/fediverse-instance.js
@@ -2,11 +2,12 @@ module.exports = function (RED) {
   function InstanceClient (n) {
     // Node  to create the shared connection to Mastodon
     RED.nodes.createNode(this, n)
+    this.kind = n.kind
     this.api_url = n.apiurl
     this.access_token = this.credentials.accesstoken
+    
     var megalodon = require('megalodon')
-
-    this.client = megalodon.default('mastodon', this.api_url, this.access_token)
+    this.client = megalodon.default(this.kind, this.api_url, this.access_token)
     console.log('Instance client created, API URL: ' + this.api_url)
   }
   RED.nodes.registerType('fediverse-instance', InstanceClient, {
